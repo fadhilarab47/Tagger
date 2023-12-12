7e917c4695c95b68c6670484bfe1e7ce2c4c3631
sɪ ᴧꝛᴧʙ <127276984+fadhilarab47@users.noreply.github.com>
2023-12-12 15:26:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,10 +97,10 @@ async def mentionall(event):
     if not chat_id in spam_chats:
       break
     usrnum += 1
-    usrtxt += f"[{random.choice(emoji_tag)}](tg://user?id={usr.id}) \n"
+    usrtxt += f"[{random.choice(emoji_tag)}](tg://user?id={usr.id})"
     if usrnum == 5:
       if mode == "text_on_cmd":
-        txt = f"{usrtxt}\n\n{msg}"
+        txt = f"{msg}\n\n{usrtxt}"
         await client.send_message(chat_id, txt)
       elif mode == "text_on_reply":
         await msg.reply(usrtxt)
